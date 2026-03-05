5220ab9228c9685b5c280f13562003ac2a6f239d
Kovall-Nick <dworak.niko@gmail.com>
2026-03-05 19:41:16 +0200

    Implement Main class with array generation and console outpad

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import core.basesyntax.Figure;
+import core.basesyntax.FigureSupplier;
+
+public class Main {
+    private static final int ARRAY_SIZE = 6;
+
+    public static void main(String[] args) {
+        FigureSupplier figureSupplier = new FigureSupplier();
+        Figure[] figures = new Figure[ARRAY_SIZE];
+
+        for (int i = 0; i < figures.length; i++) {
+            if (i < figures.length / 2) {
+                figures[i] = figureSupplier.getRandomFigure();
+            } else {
+                figures[i] = figureSupplier.getDefaultFigure();
+            }
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
